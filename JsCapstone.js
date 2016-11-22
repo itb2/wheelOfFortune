1bfe755c67ed70fa04a1aabf1375033bd0e5cdba
Ivory Brown <itb2@duke.edu>
2016-11-22 16:56:58 -0500

    pushing wheel of fortune

--- JsCapstone.js
+++ JsCapstone.js
@@ -190,3 +190,12 @@ function isVowel(x){
 	}
 
 }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
